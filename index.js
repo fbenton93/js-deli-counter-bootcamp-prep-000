a11d6ebc4eb064350d953e2b7d0dc3530ae6dd7f
Forrest Benton <elliottbenton@gmail.com>
2018-07-16 23:56:03 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -18,8 +18,7 @@ function currentLine(line) {
   var output = [];
   if (line.length === 0) {
     return "The line is currently empty."
-  }
-  else {
+  } else {
     for (var i = 0; i < line.length; i++) {
       output += (i+1) + ". " + line[i] + ", "
     }
